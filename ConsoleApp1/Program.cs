9d4108e76389ef2bc7a60b3fe4449891be03d869
Мария Сафи <safim@yandex.ru>
2022-12-24 19:54:32 +0500

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Home_Work_1;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Input Your Name");
+        var name = Console.ReadLine();
+
+        Console.WriteLine($"\n Привет, {name}!");
+
+        for (int i = 0; i < name.Length; i += 2)
+
+        {
+            Console.Write(name[i] + " ");
+        }
+
+
+        Console.WriteLine("!");
+
+
+        Console.Write($"Enter any key.....");
+        Console.ReadKey(true);
+    }
+
+}
